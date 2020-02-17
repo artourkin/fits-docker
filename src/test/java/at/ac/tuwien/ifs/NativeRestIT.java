db0570a7b20dc09f1d78317e74f4ceaffdf7dbe0
artourkin <artur.kulmukhametov@tuwien.ac.at>
2020-02-17 23:27:48 +0100

    cleanup

--- src/test/java/at/ac/tuwien/ifs/NativeRestIT.java
+++
@@ -1,9 +0,0 @@
-package at.ac.tuwien.ifs;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeRestIT extends RestTest {
-
-    // Execute the same tests but in native mode.
-}
